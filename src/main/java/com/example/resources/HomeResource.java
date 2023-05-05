3bafab70c150b9be62d6d841d6ea2469c3fdc9b4
James Kebinger <james.kebinger@prefab.cloud>
2023-05-05 16:38:33 -0500

    Add example logging

--- src/main/java/com/example/resources/HomeResource.java
+++ src/main/java/com/example/resources/HomeResource.java
@@ -3,6 +3,8 @@ package com.example.resources;
 import com.example.auth.CustomAuthFilter;
 import com.example.auth.UserDatabase;
 import com.example.views.HomeView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
@@ -14,9 +16,16 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.TEXT_HTML)
 @Path("/")
 public class HomeResource {
+    private static final Logger LOG = LoggerFactory.getLogger(HomeResource.class);
 
     @GET
     public HomeView home(@Context HttpServletRequest request){
+        LOG.debug("🔍 Hello debug logger");
+        LOG.info("ℹ️ Hello info logger");
+        LOG.warn("⚠️ Hello warn logger");
+        LOG.error("🚨 Hello error logger");
+
+
         return new HomeView()
                 .setCurrentUser(CustomAuthFilter.getUserFromSession(request.getSession()).orElse(UserDatabase.JEFF))
                 .setAllUsers(UserDatabase.ALL_USERS);
